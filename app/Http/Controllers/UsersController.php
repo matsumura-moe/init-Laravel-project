4f4b5fc8f76d1a6f8a116347b1939ffef61ae6a0
moe.matsumura <matsumura_m@so-labo.co.jp>
2024-10-15 15:33:39 +0900

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;                        // 追加 認証
+use App\Models\User;                                        // 追加　ユーザ情報
+
+class UsersController extends Controller
+{
+    public function index()                                 // 追加 ユーザ一覧      
+    {                                                       // 追加
+        // ユーザ一覧をidの降順で取得
+        $users = User::orderBy('id', 'desc')->paginate(10); // 追加
+
+        // ユーザ一覧ビューでそれを表示
+        return view('users.index', [                        // 追加
+            'users' => $users,                              // 追加
+        ]);                                                 // 追加
+    }                                                       // 追加
+    
+    public function show($id)                               // 追加　個別詳細ページ
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // ユーザ詳細ビューでそれを表示
+        return view('users.show', [
+            'user' => $user,
+        ]);                                                 // 追加
+    }                                                       // 追加
+}
