9f34351f23989b5a910ef8b5263fd9c2717bdcdf
moe.matsumura <matsumura_m@so-labo.co.jp>
2024-10-21 18:30:36 +0900

    favorite-button_change

--- resources/views/idol_favorite/favorite_button.blade.php
+++ resources/views/idol_favorite/favorite_button.blade.php
@@ -5,13 +5,13 @@
             @csrf
             @method('DELETE')
             <button type="submit" class="btn btn-error btn-block normal-case" 
-                onclick="return confirm('id = {{ $idol->id }} を推しから外します。よろしいですか？')">Unfavorite</button>
+                onclick="return confirm('id = {{ $idol->id }} を推しから外します。よろしいですか？')">推し解除</button>
         </form>
     @else
         {{-- favoriteボタンのフォーム --}}
         <form method="POST" action="{{ route('favorites.favorite', $idol->id) }}">
             @csrf
-            <button type="submit" class="btn btn-primary btn-block normal-case">favorite</button>
+            <button type="submit" class="btn btn-primary btn-block normal-case">推し</button>
         </form>
     @endif
 @endif
